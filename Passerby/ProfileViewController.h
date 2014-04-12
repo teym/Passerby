a03fb70148c650448fcfd204a09da604f9245065
Mike9058 <ccteym@gmail.com>
2014-04-12 16:45:43 +0800

    ui

---
+++ Passerby/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Passerby
+//
+//  Created by Mike on 14-4-11.
+//  Copyright (c) 2014年 xiami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
